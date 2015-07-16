ee2839629b0c8155007b6a73117352cdf8c396ff
iOS <ios@MAC-129.nabs.com.br>
2015-07-16 17:33:12 -0300

    Primeiro commit

---
+++ ovo/Controllers/CustomNavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomNavigationController.swift
+//  ovo
+//
+//  Created by iOS on 7/15/15.
+//
+//
+
+import Foundation
---
+++ ovo/Views/EpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell.swift
+//  ovo
+//
+//  Created by iOS on 7/16/15.
+//
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
